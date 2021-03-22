d0a3f284865a15c7e4e3769e9b8031191aa9d0f3
devangelista2 <davide.evangelista5@unibo.it>
2021-03-22 18:00:47 +0100

    Added comments.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import data
 
+"""
+This file contains an example on how to generate the dataset from the functions in this library.
+"""
+
 ellipsoid_data = data.get_data()
 data.to_tif(ellipsoid_data)
 
