d5982630d04b41503b4fbbb835d9071cccc8c32d
devangelista2 <davide.evangelista5@unibo.it>
2021-03-24 12:33:46 +0100

    Fixed bugs

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ import numpy as np
 This file contains an example on how to generate the dataset from the functions in this library.
 """
 
-PATH = './SparseCT/data/'
+PATH = './SparseCT/'
 LOAD = False
 
 if not LOAD:
-    ellipsoid_data = data.get_data()
+    ellipsoid_data = data.get_data((1, 450, 600, 32))
     data.to_tif(ellipsoid_data, PATH)
     data.to_tif(ellipsoid_data, PATH, type='parallel')
     data.to_np(ellipsoid_data, PATH)
 else:
     ellipsoid_data = np.load(PATH + 'ellipsoid_dataset.npy')
     print(ellipsoid_data.shape)
+
+s = transform.img_to_sino(np.transpose(ellipsoid_data[0], (2, 0, 1)))
